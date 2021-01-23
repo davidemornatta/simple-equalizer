876e6dba73928b9eabaebaa1cdf330f1986e83d8
davidemornatta <davidemornatta@gmail.com>
2021-01-23 15:50:58 +0100

    other : improved basic gui and little tweaks to the equalization algorithm

--- PyImageEqualizer.py
+++ PyImageEqualizer.py
@@ -1,32 +1,33 @@
-import PIL
-import numpy
-import tkinter as tk
 import math
-from PIL import Image
-from tkinter.filedialog import askopenfilename
-from numpy import asarray
-from numpy import array
-from numpy import log2
+import tkinter as tk
+import numpy as np
 from math import floor
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from PIL import Image
+from numpy import asarray
+from numpy import log2
+
 
 def open_file():
     """Open an image to equalize"""
     filepath = askopenfilename(
-        filetypes=[("Image", "*.png *.jpg"),("PNG", "*.png"), ("JPEG", "*.jpg")]
+        filetypes=[("Image", "*.png *.jpg"), ("PNG", "*.png"), ("JPEG", "*.jpg")]
     )
     if not filepath:
         return
     entry.delete("0", tk.END)
     entry.insert(tk.END, filepath)
 
+
 def convert():
     """Convert using a semplified version of the Histogram Equalization"""
     filepath = entry.get()
     to_equalize = Image.open(filepath)
     to_equalize = to_equalize.convert('L')
-    to_equalize.show()
+    # to_equalize.show()
     data = asarray(to_equalize)
-    equalizedArray = numpy.empty_like(data);
+    equalizedArray = np.empty_like(data)
 
     max_value = 0
     min_value = 0
@@ -34,50 +35,56 @@ def convert():
 
     for i in range(int(data.size / data[0].size)):
         for j in range(data[0].size):
-            if data[i][j] > max_value: max_value = data[i][j]
-            if data[i][j] < min_value: min_value = data[i][j]
+            if data[i][j] > max_value:
+                max_value = data[i][j]
+            if data[i][j] < min_value:
+                min_value = data[i][j]
             average = average + data[i][j]
 
-    average = int(average/data.size)
+    average = int(average / data.size)
     delta_value = max_value - min_value
     shift_level = 8 - floor(log2(delta_value + 1))
 
     for i in range(int(data.size / data[0].size)):
-        row = numpy.empty_like(data[0])
+        row = np.empty_like(data[0])
         for j in range(data[0].size):
-            if average<123:
+            if average < 123:
                 temp_pixel = int((data[i][j] - min_value) * math.pow(2, shift_level))
-            else : temp_pixel = int((data[i][j] - min_value) / math.pow(2, shift_level))
-            new_pixel = max(0,min(255, temp_pixel))
+            else:
+                temp_pixel = int((data[i][j] + min_value) / math.pow(2, shift_level))
+            new_pixel = max(0, min(255, temp_pixel))
             row[j] = new_pixel
         equalizedArray[i] = row
 
     equalized = Image.fromarray(equalizedArray)
-    equalized.show()
+    equalized.save("Equalized.png", "PNG")
+    # equalized.show()
 
+
+"""GUI handling with Tkinter"""
 window = tk.Tk()
-window.title("SimpleEqualizer 1.0")
+window.geometry("400x400")
+window.title("PyImageEqualizer 1.0")
 entry = tk.Entry(fg="black", bg="white", width=50)
 pathButton = tk.Button(
     text="Open File",
-    width=20,
-    height=10,
+    width=7,
+    height=2,
     bg="white",
     fg="black",
     command=open_file,
 )
 convertButton = tk.Button(
     text="Equalize",
-    width=80,
-    height=20,
+    width=50,
+    height=4,
     bg="white",
     fg="black",
     command=convert,
 )
 
-
-
-entry.pack()
-pathButton.pack()
-convertButton.pack()
+entry.place(x=20, y=20)
+entry.insert(0, "Paste image path here...")
+pathButton.place(x=325, y=10)
+convertButton.place(x=20, y=60)
 window.mainloop()
